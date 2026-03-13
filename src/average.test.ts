d252503797113d5eb077e050520c6aca20a6805f
Andri Grünig <andri_gruenig@sluz,ch>
2026-03-13 14:45:59 +0100

    Fix expected value in mean test case

--- src/average.test.ts
+++ src/average.test.ts
@@ -3,7 +3,7 @@ import { mean } from "./average";
 test("test mean of two numbers", () => {
   // Arrange
   const numbers: number[] = [2, 4];
-  const expected: number = 2.5;
+  const expected: number = 3;
 
   // Act
   const actual: number = mean(numbers);
