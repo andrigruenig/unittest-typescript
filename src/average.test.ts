677d20bf39d2c93aa9f8aeafb433444ea8c9dfab
Andri Grünig <andri_gruenig@sluz,ch>
2026-03-13 14:44:39 +0100

    changed numbers

--- src/average.test.ts
+++ src/average.test.ts
@@ -2,7 +2,7 @@ import { mean } from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
-  const numbers: number[] = [1, 4];
+  const numbers: number[] = [2, 4];
   const expected: number = 2.5;
 
   // Act
