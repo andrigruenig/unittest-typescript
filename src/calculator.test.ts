76eeee3ac799406ba023a51e2d0b860c5a361517
Andri Grünig <andri_gruenig@sluz,ch>
2026-03-13 14:35:30 +0100

    changed variable

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -2,7 +2,7 @@ import { add, subtract } from "./calculator";
 
 test("add three to two is five", () => {
   // Arrange
-  const a: number = 2;
+  const a: number = 4;
   const b: number = 3;
   const expected: number = 5;
 
